9ac19ebeeb86226981746d0ab43cb071fa927b3c
ankitsde2 <ankitsde2@gmail.com>
2026-04-20 08:48:55 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,38 @@
+class Solution {
+    private boolean isAnagram(String s, String t){
+        int ns=s.length();
+        int ts=t.length();
+        if(ns!=ts) return false;
+        int[] freq= new int[26];
+        for(int i=0;i<ns;i++){
+            freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0) return false; 
+        }
+        return true;
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // Brute Force
+        // t-> O(n^3), s->O(n)
+        int n=strs.length;
+        boolean[] included= new boolean[n];
+        List<List<String>> result=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(included[i]) continue;
+            included[i]=true;
+            List<String> anagram= new ArrayList<>();
+            anagram.add(strs[i]);
+            for(int j=i+1;j<n;j++){
+                if(included[j]) continue;
+                if(isAnagram(strs[i],strs[j])){
+                    anagram.add(strs[j]);
+                    included[j]=true;
+                }
+            }
+            result.add(anagram);
+        }
+        return result;
+    }
+}
