edb8ab5279922f215c78cd96a89d8a806b95d7ff
ankitsde2 <ankitsde2@gmail.com>
2026-05-08 06:55:41 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,42 @@
+class MinStack {
+    class Pair {
+        int value;
+        int min;
+        Pair(int value, int min) {
+            this.value = value;
+            this.min = min;
+        }
+    }
+    Stack<Pair> st;
+    public MinStack() {
+        st = new Stack<>();
+    }
+
+    public void push(int val) {
+        if(st.isEmpty()){
+            st.push(new Pair(val, val));
+        }
+        else{
+            int min=Math.min(st.peek().min, val);
+            st.push(new Pair(val, min));
+        }
+    }
+
+    public void pop() {
+        st.pop();
+    }
+
+    public int top() {
+        if (!st.isEmpty()) {
+            return st.peek().value;
+        }
+        return -1;
+    }
+
+    public int getMin() {
+        if (!st.isEmpty()) {
+            return st.peek().min;
+        }
+        return -1;
+    }
+}
